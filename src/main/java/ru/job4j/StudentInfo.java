f884fac32ccb387ebfc9b2f7747470dc2732e6ac
goodtoseeyou <sitcom@narod.ru>
2024-05-15 16:12:36 +0300

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Andrey Primachenko");
         System.out.println("05.03.1971");
     }
